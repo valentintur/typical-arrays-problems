4a7b8e082d30efc21c0166e8c7a27c87c487d9c3
macbook <macbook@MacBook-Pro-macbook.local>
2020-02-26 02:02:01 +0300

    init: star typical-array task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let minimal = array[0];
+  for (let i = 1; i < array.length; i++) {
+     if (array[i] < minimal)  minimal = array[i];   
+  }
+  console.log(minimal);
+  return minimal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let maximum = array[0];
+  
+  for (let i = 1; i < array.length; i++) {
+     if (array[i] > maximum )  maximum  = array[i];   
+  }
+  console.log(maximum);
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let average = 0;
+  let summ = 0;
+  for (let i = 0; i < array.length; i++) {
+     summ = summ + array[i];  
+  }
+  console.log(summ);
+  average = summ / array.length ;
+  return average;
 }
